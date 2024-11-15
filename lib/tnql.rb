e7d741bbab77fed001f41283b30e666e6467dc6c
Brian Shand <Brian.Shand@nhs.net>
2024-11-15 12:16:33 +0000

    Fix typo

--- lib/tnql.rb
+++ lib/tnql.rb
@@ -2,7 +2,7 @@ require 'active_support/deprecation'
 begin
   require 'active_support/deprecator'
 rescue LoadError
-  # Ignore error: 'active_support/deprecator'' is not defined on active_support 7.0
+  # Ignore error: 'active_support/deprecator' is not defined on active_support 7.0
 end
 require 'active_support/time'
 
